b1738a7b97cf4ea5a6c353f5d921119c7424f1cb
TinNguyen <ductin16111@gmail.com>
2023-12-24 06:36:11 +0200

    add icon in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,34 @@
 import ConnectButton from "./ConnectButton"
 import bird from '../img/bird.png'
+import linkedin from '../img/linkedin.svg'
+import github from '../img/github.svg'
 import { motion } from "framer-motion"
 
 const Header = () => {
+    const iconVariants = {
+        initial: { borderRadius: '50%' },
+        hover: {
+            scale: 1.15,
+            backgroundColor: '#fff',
+        }
+    }
     return (
         <div className="header">
-            <ConnectButton />
+            <div className="header-btn-ctn" >
+                <ConnectButton />
+                <a href="https://www.linkedin.com/in/tin-nguyen-01600/" target="_blank" rel="noopener noreferrer">
+                    <motion.img
+                        src={linkedin} alt="LinkedIn" 
+                        variants={iconVariants} initial='initial' whileHover='hover'
+                    />
+                </a>
+                <a href="https://github.com/TinNguyen01600" target="_blank" rel="noopener noreferrer">
+                    <motion.img
+                        src={github} alt="Github" 
+                        variants={iconVariants} initial='initial' whileHover='hover'
+                    />
+                </a>
+            </div>
             <motion.img src={bird} alt=""
                 className="bird"
                 animate={{ y: [-5, 5, -5] }}
