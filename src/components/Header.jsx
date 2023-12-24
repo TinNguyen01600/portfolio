1ff24dd182ebe37ca045af012ffea8bf133dd645
TinNguyen <ductin16111@gmail.com>
2023-12-24 05:25:30 +0200

    add Header image and animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 import ConnectButton from "./ConnectButton"
+import bird from '../img/bird.png'
+import { motion } from "framer-motion"
 
 const Header = () => {
     return (
-        <>
+        <div className="header">
             <ConnectButton />
-        </>
+            <motion.img src={bird} alt=""
+                className="bird"
+                animate={{ y: [-5, 5, -5] }}
+                transition={{
+                    duration: 3,
+                    repeat: Infinity,
+                    ease: "linear",
+                }}
+            />
+        </div>
     )
 }
 
