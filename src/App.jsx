a4850ebd133267e0eb8d95d778d9292c82409233
TinNguyen <ductin16111@gmail.com>
2023-12-27 20:15:06 +0200

    divide page into sections and create component Parallax.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import Banner from './components/Banner'
 import Header from './components/Header'
+import Parallax from './components/Parallax'
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Banner />
-    </>
-  )
+    return (
+        <>
+            <section className='section-1'>
+                <Header />
+                <Banner />
+            </section>
+            <section className='section-2'>
+                <Parallax />
+            </section>
+        </>
+    )
 }
 
 export default App
---
+++ src/components/Parallax.jsx
@@ -0,0 +1,12 @@
+const Parallax = () => {
+    return (
+        <div className="parallax">
+            <h1>Nothing Special Here</h1>
+            <h1>Just some cool animation</h1>
+            <div className="earth"></div>
+            <div className="stars"></div>
+        </div>
+    )
+}
+
+export default Parallax
