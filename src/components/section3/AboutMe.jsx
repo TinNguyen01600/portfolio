fc5f37453078067e8a91647d72fb09ba8861e974
TinNguyen <ductin16111@gmail.com>
2024-01-02 21:37:46 +0200

    update component AboutMe.jsx

--- src/components/section3/AboutMe.jsx
+++ src/components/section3/AboutMe.jsx
@@ -1,18 +1,31 @@
 import { Link } from "react-router-dom";
+import me from '../../img/section3/me.jpg'
 
 const AboutMe = () => {
     return (
         <div className="about-me">
-            <p>Hello! My name is Tin Nguyen. 
-                I have background in embedded system C/C++, python and automation engineer. 
-                I’ve worked hard in my education, and got considerably good grades. I’m especially confident in mathematics and physics.
-                About myself, I’m a flexible and a quick learner. I’ve always been good with 
-                numbers and electronic integration. I also have high ability to work within 
-                groups and corporate effectively with other people.
-            </p>
-            <Link to="/">
+            <img src={me} alt="" />
+            <div className="text">
+                <h1>Hello! My name is Tin Nguyen.</h1>
+                <h3>Your friendly neighborhood software engineer</h3>
+                <ul>
+                <li>
+                    Born and raised in Ho Chi Minh city, Vietnam,
+                    I discovered my interest in maths at a very young age. That,
+                    though many years, grew up into the enthusiasm for technical
+                    and information technology. My passion brought me to Vaasa, Finland,
+                    where I completed my IT bachelor's degree program.
+                </li>
+                <li>
+                    In my college, I'm usually considered a flexible and a
+                    quick learner. I also have high ability to work within groups
+                    and corporate effectively with other people.
+                </li>
+                </ul>
+            </div>
+            {/* <Link to="/">
                 <button>back</button>
-            </Link>
+            </Link> */}
         </div>
     )
 }
