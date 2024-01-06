acc2afbcc78604ac2ad2efe4008a5737a9fe508b
TinNguyen <ductin16111@gmail.com>
2024-01-06 21:14:03 +0200

    update component Education.jsx

--- src/components/section3/Education.jsx
+++ src/components/section3/Education.jsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import CloseButton from "./CloseButton";
+import degree from '../../img/section3/degree.svg'
+import certi from '../../img/section3/certi.svg'
 
 const variants = {
     open: {
@@ -16,7 +18,31 @@ const Education = () => {
         <motion.div className="education" animate="open">
             <motion.div className="bg" variants={variants}>
                 <div className="main">
-                    Education
+                    <div className="upper">
+                        <div className="item">
+                            Bachelor Degree in Information Technology
+                        </div>
+                        <div className="item">
+                            Network Security
+                        </div>
+                    </div>
+                    {/***************************************************** */}
+                    <div className="middle">
+                        <img src={degree} alt="" />
+                        <svg height='50' width='100'>
+                            <line x1="0" y1="30" x2="100" y2="30" stroke="black" strokeWidth='4' />
+                        </svg>
+                        <img src={certi} alt="" />
+                    </div>
+                    {/***************************************************** */}
+                    <div className="lower">
+                        <div className="item">
+                            Python Bootcamp
+                        </div>
+                        <div className="item">
+                            AI - Machine Learning
+                        </div>
+                    </div>
                 </div>
                 <CloseButton />
             </motion.div>
