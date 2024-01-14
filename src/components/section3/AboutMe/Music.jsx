e221461ff8875f8153a447aa441757d03428adc5
TinNguyen <ductin16111@gmail.com>
2024-01-14 17:30:21 +0200

    add CSS file for Music.jsx

--- src/components/section3/AboutMe/Music.jsx
+++ src/components/section3/AboutMe/Music.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
-import movie from '../../../img/section3/aboutme/movie.jpg'
-import '../../../CSS/section3/AboutMe/Cinephile.css'
+import '../../../CSS/section3/AboutMe/Music.css'
+import music from '../../../img/section3/aboutme/music.svg'
 
 const Music = (props) => {
     return (
         <div {...props}>
-            <div className='cinephile'>
-                <div className="video">
-                    <iframe width="420" height="315"
-                        src="https://www.youtube.com/embed/Ui5UxquV-Ds">
-                    </iframe>
-                </div>
+            <div className='music'>
                 <div className="text">
-                    <h1>Music <mark>Lover</mark>.</h1>
+                    <h1>
+                        <p>Music <mark>Lover</mark></p>
+                        <img src={music} alt="" />
+                    </h1>
                     <p>
                         Besides, I'm also keen on arts, especially music and drawing.
                         I can play guitar passably, and enjoy it. I usually practice guitar in my spare time,
                         mostly on Youtube and this commandably website Songsterr.
                     </p>
+                </div>
+                <div className="video">
+                    <iframe width="650vw" height='400vh'
+                        src="https://www.youtube.com/embed/Ui5UxquV-Ds">
+                    </iframe>
                 </div>
             </div>
         </div>
