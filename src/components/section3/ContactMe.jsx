92818e57004128c6e7a730dc9026be9fef166811
TinNguyen <ductin16111@gmail.com>
2024-01-15 14:10:15 +0200

    add new animation for component ContactMe.jsx

--- src/components/section3/ContactMe.jsx
+++ src/components/section3/ContactMe.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion'
 import CloseButton from './CloseButton';
 import '../../CSS/ContactMe.css'
 
-const variants = {
+const variants1 = {
     open: {
         clipPath: 'circle(1000px)',
         transition: {
@@ -12,12 +12,32 @@ const variants = {
     },
 };
 
+const variants2 = {
+    initial: {
+        y: 500,
+        opacity: 0
+    },
+    animate: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.5,
+            staggerChildren: 0.1
+        }
+    }
+}
+
 const ContactMe = () => {
     return (
         <motion.div className="contact-me" animate="open">
-            <motion.div className="bg" variants={variants}>
-                <div className='main'>
-                    <div className="text-ctn">
+            <motion.div className="bg" variants={variants1}>
+                <motion.div
+                    className='main'
+                    variants={variants2}
+                    initial='initial'
+                    whileInView='animate'
+                >
+                    <motion.div className="text-ctn" variants={variants2}>
                         <h1>Let's work together</h1>
                         <div>
                             <h2>Email</h2>
@@ -31,16 +51,16 @@ const ContactMe = () => {
                             <h2>LinkedIn</h2>
                             <span>tinnguyen01600</span>
                         </div>
-                    </div>
-                    <div className="form-ctn">
+                    </motion.div>
+                    <motion.div className="form-ctn" variants={variants2}>
                         <form action="">
                             <input type="text" required placeholder='Name' />
                             <input type="email" required placeholder='Email' />
                             <textarea rows={10} placeholder='Message'></textarea>
                             <button>Submit</button>
                         </form>
-                    </div>
-                </div>
+                    </motion.div>
+                </motion.div>
                 <CloseButton />
             </motion.div>
         </motion.div>
